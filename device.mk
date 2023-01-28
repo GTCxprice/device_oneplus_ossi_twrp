4c6e4e38b030de8c7feb3d166fb8f7cc170ac322
GTCxprice <letsrockwithshreyasstark@gmail.com>
2023-01-28 05:55:59 +0000

    Change in BoardConfig device.mk recovery & prebuilt

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 
 LOCAL_PATH := device/oneplus/ossi
 
-# API
-PRODUCT_SHIPPING_API_LEVEL := 31
-
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -51,6 +48,17 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl
 
@@ -63,6 +71,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
 
 # Keymint
 PRODUCT_PACKAGES += \
@@ -81,6 +93,9 @@ PRODUCT_PACKAGES += \
 # Additional target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1
+
+#TARGET_INIT_VENDOR_LIB := libinit_ossi
+#TARGET_RECOVERY_DEVICE_MODULES := libinit_ossi
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so
